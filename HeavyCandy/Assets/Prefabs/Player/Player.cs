9f32fec7472a64630d8672507fc07feca4007b5b
Erik Mansén <erima668@student.liu.se>
2018-11-17 00:31:17 +0100

    Add Prefabs folder

---
+++ HeavyCandy/Assets/Prefabs/Player/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public int cameraSpeed = 4;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        UpdateCamera();
+
+    }
+
+    void UpdateCamera() {
+        Vector3 cameraMovement = new Vector3(0, 0, 0);
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
+            cameraMovement.z += cameraSpeed;
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+            cameraMovement.x -= cameraSpeed;
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
+            cameraMovement.z -= cameraSpeed;
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+            cameraMovement.x += cameraSpeed;
+        }
+        transform.Translate(cameraMovement);
+    }
+}
