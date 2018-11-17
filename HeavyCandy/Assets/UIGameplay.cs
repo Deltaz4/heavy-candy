fad5288b2e7964ce519fc244c07f8f208628cdec
MattiasL2001 <mattias-lindblad2001@hotmail.com>
2018-11-17 15:58:25 +0100

    UI + UI Script

---
+++ HeavyCandy/Assets/UIGameplay.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIGameplay : MonoBehaviour
+{
+
+    public TextMeshProUGUI PoliceOperaText;
+    public Slider PoliceOperaSlider;
+
+    public TextMeshProUGUI PoliceMetalText;
+    public Slider PoliceMetalSlider;
+
+    public TextMeshProUGUI PoliceDiscoText;
+    public Slider PoliceDiscoSlider;
+
+    public TextMeshProUGUI BuyersOperaText;
+    public Slider BuyersOperaSlider;
+
+    public TextMeshProUGUI BuyersMetalText;
+    public Slider BuyersMetalSlider;
+
+    public TextMeshProUGUI BuyersDiscoText;
+    public Slider BuyersDiscoSlider;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
