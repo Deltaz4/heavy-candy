7b9900c4a138a7864031bf5ee1f08803dec0a11a
Rasmus Larsson <rasmus.larsson@proxymine.se>
2018-11-17 13:16:29 +0100

    Moved shared methods to base class Unit

--- HeavyCandy/Assets/Prefabs/Unit/Band/Band.cs
+++ HeavyCandy/Assets/Prefabs/Unit/Band/Band.cs
@@ -5,9 +5,6 @@ using UnityEngine;
 public class Band : Unit {
 
     public Transform startingPoint;
-    public House house;
-    public Collider collisionGameObject;
-    public float destinationHitRadius;
 
 	void Start () {
         base.Initialize();
@@ -16,26 +13,15 @@ public class Band : Unit {
         SetDestination(house);
 	}
 
-    public void SetDestination(House house) {
+    public void SetDestination(House house)
+    {
         base.SetDestination(house.transform.position);
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    void Update () {
         if (atDestination()) {
             // Do something! Resets to starting position for now.
             transform.position = startingPoint.position;
         }
 	}
-
-    private bool atDestination() {
-        Vector3 closestSurfacePoint1;
-        Vector3 closestSurfacePoint2;
-
-        closestSurfacePoint1 = collisionGameObject.ClosestPointOnBounds(house.transform.position);
-        closestSurfacePoint2 = house.GetComponent<Collider>().ClosestPointOnBounds(transform.position);
-
-        float surfaceDistance = Vector3.Distance(closestSurfacePoint1, closestSurfacePoint2);
-        return (surfaceDistance < destinationHitRadius);
-    } 
 }
--- HeavyCandy/Assets/Prefabs/Unit/Unit.cs
+++ HeavyCandy/Assets/Prefabs/Unit/Unit.cs
@@ -6,6 +6,9 @@ using UnityEngine.AI;
 abstract public class Unit : MonoBehaviour {
 
     NavMeshAgent agent;
+    public Collider collisionGameObject;
+    public House house;
+    public float destinationHitRadius;
 
     /// <summary>
     /// Sets the NavMeshAgent of the implementing class.
@@ -16,5 +19,17 @@ abstract public class Unit : MonoBehaviour {
 
     protected void SetDestination(Vector3 destination) {
         agent.destination = destination;
+    }
+
+    protected bool atDestination()
+    {
+        Vector3 closestSurfacePoint1;
+        Vector3 closestSurfacePoint2;
+
+        closestSurfacePoint1 = collisionGameObject.ClosestPointOnBounds(house.transform.position);
+        closestSurfacePoint2 = house.GetComponent<Collider>().ClosestPointOnBounds(transform.position);
+
+        float surfaceDistance = Vector3.Distance(closestSurfacePoint1, closestSurfacePoint2);
+        return (surfaceDistance < destinationHitRadius);
     }
 }
