0c43f418be0aba4d97d9acc9358795f36d09fde9
Rasmus Larsson <rasmus.larsson@proxymine.se>
2018-11-17 12:10:10 +0100

    Band can now detect if they have arrived at destination

--- HeavyCandy/Assets/Prefabs/Unit/Band/Band.cs
+++ HeavyCandy/Assets/Prefabs/Unit/Band/Band.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class Band : Unit {
 
+    public Transform startingPoint;
     public House house;
-    public GameObject collisionChild;
+    public Collider collisionGameObject;
+    public float destinationHitRadius;
 
 	void Start () {
         base.Initialize();
+
+        // Remove
+        SetDestination(house);
 	}
 
     public void SetDestination(House house) {
@@ -17,10 +22,20 @@ public class Band : Unit {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (atDestination()) {
+            // Do something! Resets to starting position for now.
+            transform.position = startingPoint.position;
+        }
 	}
 
     private bool atDestination() {
-        return false;
+        Vector3 closestSurfacePoint1;
+        Vector3 closestSurfacePoint2;
+
+        closestSurfacePoint1 = collisionGameObject.ClosestPointOnBounds(house.transform.position);
+        closestSurfacePoint2 = house.GetComponent<Collider>().ClosestPointOnBounds(transform.position);
+
+        float surfaceDistance = Vector3.Distance(closestSurfacePoint1, closestSurfacePoint2);
+        return (surfaceDistance < destinationHitRadius);
     } 
 }
