ebb16e0a33b093a62b18cfd75319eda0295fc406
Axel Nordanskog <axeno840@student.liu.se>
2018-11-18 01:31:10 +0100

    Use van sprites in-game

--- HeavyCandy/Assets/Prefabs/Unit/Band/Band.cs
+++ HeavyCandy/Assets/Prefabs/Unit/Band/Band.cs
@@ -6,8 +6,11 @@ public class Band : Unit {
 
     private bool playing = false;
 
+    private GameObject sprite;
+
 	void Awake () {
         base.Initialize();
+        sprite = transform.Find("Sprite").gameObject;
 	}
 
     public void SetDestination(House house)
@@ -15,11 +18,20 @@ public class Band : Unit {
         base.SetDestination(house.transform.position);
     }
 
-    void Update () {
+    void Update ()
+    {
+        sprite.GetComponent<UnitSprite>();
+        sprite.GetComponent<UnitSprite>().SetRotation(transform.rotation.eulerAngles.y);
+
         if (AtDestination()) {
             playing = true;
             //Activate house animation
 
         }
 	}
+
+    private void LateUpdate()
+    {
+        sprite.transform.forward = Camera.main.transform.forward;
+    }
 }
