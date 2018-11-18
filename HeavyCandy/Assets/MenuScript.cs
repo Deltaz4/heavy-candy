ef08cda008f9eeb2751306280293f09160f4b9fb
MattiasL2001 <mattias-lindblad2001@hotmail.com>
2018-11-18 15:54:21 +0100

    Exported folder + StartGame Scéne

---
+++ HeavyCandy/Assets/MenuScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public int LevelIndex;
+
+    public void StartGame ()
+    {
+        SceneManager.LoadScene(LevelIndex);
+    }
+}
