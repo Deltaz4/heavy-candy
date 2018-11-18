2cbf9628ac2777e59b63a9afd90fe9a3605595dc
Rasmus Larsson <rasmus.larsson@proxymine.se>
2018-11-18 13:17:50 +0100

    Added a counter for successful police stings, and a game over counter

--- HeavyCandy/Assets/Prefabs/PoliceController/PoliceController.cs
+++ HeavyCandy/Assets/Prefabs/PoliceController/PoliceController.cs
@@ -10,26 +10,63 @@ public class PoliceController : MonoBehaviour {
     FactionLogic factionLogic;
     public float policeSpawnInterval;
     float timeSinceLastSpawn;
+    int successfulPoliceStings; // Will never decay, as opposed to gameOverMeter
+
+
+    int gameOverMax; // The value of gameOverMeter at which the game is over
+    int gameOverMeter; // A value between 0 and <gameOverMax>, used to determine how close the police are to busting the HQ 
+    public int policeStingMultiplier; // How much each successful police sting will add to gameOverMeter
+    public float gameOverDecayInterval; // Interval in seconds at which the gameOverMeter is reduced by 1. (Never reduced if interval is set to 0)
+    float timeSinceLastDecay;
 
     void Start() {
         factionLogic = new FactionLogic();
-        
+        successfulPoliceStings = 0;
+        gameOverMax = 100;
+        gameOverMeter = 0;
     }
 
     void Update () {
         timeSinceLastSpawn += Time.deltaTime;
         if (timeSinceLastSpawn > policeSpawnInterval) {
-            FactionLogic.Genre targetGenre = PickGenre();
-            House targetHouse = PickHouse(targetGenre);
-
-            //Debug.Log(string.Format("Genre: {0}, House: {1}", targetGenre, targetHouse));
-
-            if (targetHouse != null) { 
-                // Problematic if no houses are partying with the picked genre
-                // Can be solved by adjusting the PickGenre() method.
-                policeStation.DeployPoliceSquad(targetGenre, targetHouse);
-            } 
+            GeneratePoliceSquad();
             timeSinceLastSpawn -= policeSpawnInterval;
+        }
+
+        if (gameOverDecayInterval != 0) {
+            timeSinceLastDecay += Time.deltaTime;
+            if (timeSinceLastDecay > gameOverDecayInterval) {
+                DecayGameMeter();
+                timeSinceLastDecay -= gameOverDecayInterval;
+            }
+        }
+    }
+
+    private void GeneratePoliceSquad() {
+        FactionLogic.Genre targetGenre = PickGenre();
+        House targetHouse = PickHouse(targetGenre);
+
+        //Debug.Log(string.Format("Genre: {0}, House: {1}", targetGenre, targetHouse));
+
+        if (targetHouse != null)
+        {
+            // Problematic if no houses are partying with the picked genre
+            // Can be solved by adjusting the PickGenre() method.
+            policeStation.DeployPoliceSquad(targetGenre, targetHouse);
+        }
+    }
+
+    private void DecayGameMeter() {
+        if (gameOverMeter == 0)
+            return;
+        gameOverMeter--;
+    }
+
+    private void IncrementGameMeter() {
+        gameOverMeter += policeStingMultiplier;
+        if (gameOverMeter > gameOverMax) {
+            gameOverMeter = gameOverMax;
+            // Game over!
         }
     }
 
@@ -64,7 +101,22 @@ public class PoliceController : MonoBehaviour {
         return houses[r];
     }
 
+    public bool IsGameOver() {
+        return (gameOverMeter == gameOverMax);
+    }
+
     public void DestinationReached(FactionLogic.Genre genre, bool foundCandy) {
-        //Debug.Log(string.Format("Reached house with genre {0}", genre));
+        if (foundCandy) { 
+            successfulPoliceStings++;
+            IncrementGameMeter();
+        }
+    }
+
+    public int GetGameOverMax() {
+        return gameOverMax;
+    }
+
+    public int GetGameOverMeter() {
+        return gameOverMeter;
     }
 }
